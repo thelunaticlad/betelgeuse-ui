ee9f3f72ac8954ca045d3982d41926d76a350077
thelunaticlad <132869389+thelunaticlad@users.noreply.github.com>
2023-12-14 17:08:38 +0545

    Setting up

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,3 +1,11 @@
-export const button = () => {
-  return <div>button</div>;
+import React from "react";
+
+interface ButtonProps {
+  children: React.ReactNode;
+}
+
+const Button: React.FC<ButtonProps> = ({ children }) => {
+  return <button style={{ background: "red" }}>{children}</button>;
 };
+
+export default Button;
