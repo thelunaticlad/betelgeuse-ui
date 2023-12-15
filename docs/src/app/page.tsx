45c88a57500384eb48ef08ca68196b436dcafcc8
thelunaticlad <132869389+thelunaticlad@users.noreply.github.com>
2023-12-15 15:11:43 +0545

    Config

--- docs/src/app/page.tsx
+++ docs/src/app/page.tsx
@@ -1,12 +1,12 @@
 "use client";
 
-import Image from "next/image";
-import Button from "betelgeuse-ui";
+import { Button } from "../../../dist/src";
+
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Button>Hello</Button>
+      <Button ariaLabel="button">Hello</Button>
     </main>
   );
 }
